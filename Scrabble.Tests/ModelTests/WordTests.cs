2586caa93e8333e884ccd2853d46f3873c83978e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-07 20:55:06 -0800

    add passing spec testing letters set to Letters property

--- Scrabble.Tests/ModelTests/WordTests.cs
+++ Scrabble.Tests/ModelTests/WordTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Scrabble.Models;
+using System;
 
 namespace Scrabble.Tests
 {
@@ -31,6 +32,19 @@ namespace Scrabble.Tests
 
       // Assert
       Assert.AreEqual(newWord.Text,"wordyword");
+    }
+
+    [TestMethod]
+    public void WordConstructor_SetLettersProperty_w()
+    {
+      //Arrange
+      string sample = "word";
+      
+      //Act
+      Word newWord = new Word(sample);
+
+      // Assert
+      Assert.AreEqual(newWord.Letters.Length,4);
     }
   }
 }
