5a6af5c022fbcd51abe12756a5eb2ad43d42697b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-07 20:09:40 -0800

    add using statement for Dictionary

--- Scrabble/Models/Word.cs
+++ Scrabble/Models/Word.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections.Generic;
 
 namespace Scrabble.Models
 {
   public class Word
-  {
+  { 
+    public Word(string wordPlayed)
+    {
+
+    }
     private static Dictionary<char, int> Scores = new Dictionary<char, int>() 
     {
       {'a', 1}, 
@@ -13,17 +18,17 @@ namespace Scrabble.Models
       {'e', 1}, 
       {'f', 4}, 
       {'g', 2}, 
-      {'h', 4}
+      {'h', 4},
       {'i', 1}, 
       {'j', 8}, 
       {'k', 5}, 
       {'l', 1}, 
-      {'m', 3}
+      {'m', 3},
       {'n', 1}, 
       {'o', 1}, 
       {'p', 3}, 
       {'q', 10}, 
-      {'r', 1}
+      {'r', 1},
       {'s', 1}, 
       {'t', 1}, 
       {'u', 1}, 
